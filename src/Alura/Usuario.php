a6ae079c58032cbf9c2f65ae33a9ffd2e93b2894
Giovanni Tempobono <giovanni.tempobono@alura.com.br>
2019-06-06 12:58:16 -0300

    aula5

--- src/Alura/Usuario.php
+++ src/Alura/Usuario.php
@@ -8,11 +8,26 @@ class Usuario
     private $nome;
     private $sobrenome;
     private $senha;
+    private $tratamento;
 
-    public function __construct(string $nome, string $senha)
+    public function __construct(string $nome, string $senha, string $genero)
     {
         $this->setNomeSobrenome($nome);
         $this->validaSenha($senha);
+
+        $this->adicionaTratamentoAoSobrenome($nome, $genero);
+
+    }
+
+    private function adicionaTratamentoAoSobrenome(string $nome, string $genero)
+    {
+        if ($genero === 'M') {
+            $this->tratamento = preg_replace('/^(\w+)\b/', 'Sr.', $nome, 1);
+        }
+
+        if ($genero === 'F') {
+            $this->tratamento = preg_replace('/^(\w+)\b/', 'Srª.', $nome, 1);
+        }
     }
 
     private function setNomeSobrenome(string $nome)
@@ -57,5 +72,10 @@ class Usuario
         } else {
             $this->senha = "Senha inválida";
         }
+    }
+
+    public function getTratamento(): string
+    {
+        return $this->tratamento;
     }
 }
